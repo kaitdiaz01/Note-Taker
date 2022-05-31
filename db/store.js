2b11eef16ae001b75b62d035b3b7f89651ad486e
Kaitlyn Diaz <katediaz011@gmail.com>
2022-05-31 17:58:06 -0400

    got apiroutes to work

--- db/store.js
+++ db/store.js
@@ -1,41 +1,41 @@
-// require the util
-// util is a built in feature of node like fs
-const util = require('util');
-const fs = require('fs');
+// // require the util
+// // util is a built in feature of node like fs
+// const util = require('util');
+// const fs = require('fs');
 
-const uuidv1 = require('uuid/v1');
+// const uuidv1 = require('uuid/v1');
 
-// writeToFile 
-const writeToFile = util.promisify(fs.writeFile);
-// readToFile
-const readToFile = util.promisify(fs.readFile);
+// // writeToFile 
+// const writeToFile = util.promisify(fs.writeFile);
+// // readToFile
+// const readToFile = util.promisify(fs.readFile);
 
 
-// require the uuid/v1 package in your package json
-class Store {
-    read() {
-        return readToFile('./db.json');
+// // require the uuid/v1 package in your package json
+// class Store {
+//     read() {
+//         return readToFile('./db.json');
 
-    }
-    write(note) {
-        return writeToFile('./db.json', JSON.stringify(note))
-    }
-    // get the notes
-    getNotes() {
+//     }
+//     write(note) {
+//         return writeToFile('./db.json', JSON.stringify(note))
+//     }
+//     // get the notes
+//     getNotes() {
         
-    }
-    // add some notes
-    addNotes() {
-        const {title, text} = note;
-        const newNote = {
-            title,
-            text,
-            id: uuidv1(),
-        };
-        // somehow add to db now
+//     }
+//     // add some notes
+//     addNotes() {
+//         const {title, text} = note;
+//         const newNote = {
+//             title,
+//             text,
+//             id: uuidv1(),
+//         };
+//         // somehow add to db now
 
-    }
-    // delete notes for bonus
-}
+//     }
+    
+// }
 
-module.exports = new Store();
+// module.exports = new Store();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,35 @@
 // require the router and db items needed
 const router = require("express").Router();
-const fs = require('fs');
-const path = require('path');
 
-// set up a get/post/delete methods as responses to the database
-router.get('/notes', (req,res) => 
-    {}
-)
+const { json } = require("express/lib/response");
+// const path = require("path");
+
+const { v4: uuidv4 } = require("uuid");
+const {
+  readFromFile,
+  writeToFile,
+  readAndAppend,
+} = require("../helpers/fsUtils");
+
+// set up a get/post/ methods as responses to the database
+router.get("/notes", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+router.post("/notes", (req, res) => {
+  const { title, text, id } = req.body;
+  const newNote = {
+    title,
+    text,
+    id: uuidv4(),
+  };
+  if (req.body) {
+    readAndAppend(newNote, "./db/db.json");
+    res.json("New Note added!");
+  } else {
+    res.error("Can not add note.");
+  }
+});
 
 // export the router
 module.exports = router;
